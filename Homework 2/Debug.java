a752838a9bcfb436d4900e7b700cf215c4d6e385
BenjaminBanza <162636743+BenjaminBanza@users.noreply.github.com>
2024-09-13 13:05:48 -0400

    Homework2

--- Homework 2/Debug.java
+++ Homework 2/Debug.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public class Debug {
     public static void main(String[] args) {
 
         //for this section: are these all printing the best option? If they aren't, fix it!
@@ -7,7 +7,13 @@ public class Main {
         int var1 = 4;
         if (var1 > 4){
             System.out.println("Var1 is greater than 4");
-        }else{
+        // I will put if else just in case the number is equal to 4
+        }
+        else if (var1 ==4 ){
+            System.out.println("Number is equal to 4");
+
+        }
+        else{
             System.out.println("Var1 is less than 4");
         }
 
@@ -25,7 +31,13 @@ public class Main {
         int var3 = 5;
         if (var3 > 10){
             System.out.println("Var3 is greater than 10");
-        } else {
+        }
+        // I added else if just in case number is equal to 10
+        else if (var3==10){
+            System.out.println("The number is equal to 10");
+
+        }
+        else {
             System.out.println("Var3 is less than 10");
         }
 
@@ -33,6 +45,8 @@ public class Main {
 
 
         //for this section: why are we not entering the if statement?
+        // Because marist with capital M is not euqal with marist with lowercase m so we directly go to "else" because "else" runs when condition if is false.
+        //
         if ("Marist" == "marist"){
             System.out.println("Marist college!");
         } else{
