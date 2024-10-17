a6a4a0d1446997c62b0ae002e5baa0c5329e55b8
BenjaminBanza <162636743+BenjaminBanza@users.noreply.github.com>
2024-10-17 16:26:08 -0400

    update

--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -6,11 +6,32 @@ Take the given array and keep track of:
 3. Print out those duplicate numbers with how many times they appear
 
  */
-
 public class Main {
     public static void main(String[] args) {
-        //Don't let the size of this scare you! no matter how big an array is, it all works the same!
-        int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
-        //you cannot use the array util. Do this one by hand :(
+        int[] myArray = {10, 3, 295, 38, 20, 3, 4, 267, 2445, 10, 5566, 87, 93, 17, 10, 2, 87, 267, 3176, 3, 82};
+
+        System.out.println("Duplicate numbers and their counts:");
+        for (int i = 0; i < myArray.length; i++) {
+            int count = 0;
+            boolean a = false;
+            for (int k = 0; k < i; k++) {
+                if (myArray[k] == myArray[i]) {
+                    a = true;
+                    break;
+                }
+            }
+
+            if (!a) {
+                for (int j = 0; j < myArray.length; j++) {
+                    if (myArray[i] == myArray[j]) {
+                        count++;
+                    }
+                }
+
+                if (count > 1) {
+                    System.out.println(myArray[i] + " appears " + count + " times.");
+                }
+            }
+        }
     }
 }
