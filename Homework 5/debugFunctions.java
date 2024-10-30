b8cb61611dd2d8a95b95faf5499f4a4102d0ed55
BenjaminBanza <162636743+BenjaminBanza@users.noreply.github.com>
2024-10-29 21:22:20 -0400

    update

--- Homework 5/debugFunctions.java
+++ Homework 5/debugFunctions.java
@@ -11,7 +11,7 @@ Things that may trip you up while working on this:
 I give you two fully working method examples with and without parameters and called them for your reference :)
 */
 
-import java.util.Scanner;
+/*/import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -44,4 +44,42 @@ public class Main {
     }
 
   
+}/* */
+import java.util.Scanner;
+class methods {
+    public static int factorial ( int x ){
+        int sum = 1;
+        for ( int i = 1; i <= x; i++){
+            sum *= i;
+        }
+        return sum;
+    }
+    public static int add (int y){
+        int sum1 = 0;
+        for ( int i = 1; i <= y; i++){
+            sum1 = sum1 + i;
+        }
+        return sum1;
+    }
+    public static String reverse (String z){
+        String a = "";
+        for ( int i = z.length() - 1; i >= 0; i--){
+            a += z.charAt(i);
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number I will tell you factorial: ");
+        int a = sc.nextInt();
+        System.out.println(factorial(a));
+        System.out.println("Enter a number and I will tell you the sum of every other number: ");
+        int b = sc.nextInt();
+        System.out.println(add(b));
+        sc.nextLine();
+        System.out.println("Enter a string I will print it in reverse: ");
+        String c = sc.nextLine();
+        System.out.println(reverse(c));
+    }
 }
+
